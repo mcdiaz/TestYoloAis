f965807c3b224e93587e9da21e5ad99fdf813799
unknown <camaras-server@CAMARAS-SERVER.pladema.net>
2020-02-11 13:37:30 -0300

    csv Output and up compare

--- compare.py
+++ compare.py
@@ -7,6 +7,7 @@ import argparse
 import json
 import time
 from datetime import datetime
+import csv
 
 
 class ContainerRN:
@@ -148,21 +149,15 @@ def runAlgAis(folderTB,aisContain, threshold, folderData, folderStAis):
     """
     resultPrint="['pickup', 'bus', 'car', 'cyclist', 'human', 'truck', 'van'];\r\n3.602055072784424;21;cyclist_46.22|pickup_34.62|human_6.40|car_5.25|van_4.75|truck_1.73|bus_1.03|;human_97.03|cyclist_2.95|bus_0.02|truck_0.00|van_0.00|pickup_0.00|car_0.00|;truck_54.41|pickup_15.22|bus_12.68|human_11.82|cyclist_4.52|car_0.90|van_0.45|;human_71.68|pickup_21.36|cyclist_6.62|car_0.22|truck_0.06|van_0.03|bus_0.02|;human_57.99|truck_24.39|pickup_8.19|bus_7.55|cyclist_1.55|van_0.20|car_0.12|;human_99.81|cyclist_0.17|truck_0.01|pickup_0.00|car_0.00|bus_0.00|van_0.00|;human_93.01|cyclist_6.83|bus_0.09|pickup_0.03|car_0.02|truck_0.01|van_0.01|;cyclist_95.17|bus_3.26|truck_1.04|human_0.37|pickup_0.11|van_0.03|car_0.02|;human_58.99|bus_26.69|cyclist_7.69|van_3.99|truck_2.27|pickup_0.28|car_0.09|;human_68.59|cyclist_28.10|bus_1.55|truck_1.11|pickup_0.52|car_0.08|van_0.05|;human_99.20|cyclist_0.62|truck_0.15|pickup_0.02|bus_0.01|car_0.00|van_0.00|;human_66.44|cyclist_33.25|truck_0.18|bus_0.09|pickup_0.03|van_0.01|car_0.00|;human_71.07|bus_13.01|van_10.08|cyclist_3.86|pickup_1.20|car_0.52|truck_0.27|;human_70.94|cyclist_14.27|pickup_8.90|truck_5.27|bus_0.28|van_0.23|car_0.10|;human_31.48|truck_22.16|bus_21.64|cyclist_12.64|pickup_6.89|van_3.09|car_2.10|;truck_79.07|human_10.33|bus_4.88|cyclist_4.81|pickup_0.78|car_0.09|van_0.05|;bus_36.80|truck_27.55|human_23.75|cyclist_11.17|van_0.52|car_0.15|pickup_0.05|;truck_78.81|human_10.80|bus_3.20|van_2.77|pickup_2.56|car_1.23|cyclist_0.62|;human_32.92|van_26.99|truck_19.32|cyclist_11.62|bus_7.90|pickup_1.05|car_0.19|;human_71.53|truck_24.49|cyclist_3.78|pickup_0.11|bus_0.10|car_0.00|van_0.00|;bus_61.37|truck_19.98|cyclist_10.96|human_7.57|pickup_0.09|car_0.02|van_0.02|\r\n"
     """
-    print(resultPrint)
     resultPrint = resultPrint[int(resultPrint.find('[')):int(len(resultPrint))]
     loadLabels(resultPrint)
-    print(resultPrint,"Antes de reemplazar los saltos")
-    print(resultPrint,"resultPrint1")
     # se llama al metodo que procesa los primeros caracteres desde el primer [ hasta el proximo ] para cargar los labels
     # correspondientes a las clasificaciones que se estudiaran
     # se salta todos los caracteres hasta ']'
-    print(resultPrint,"resultPrint2")
     resultPrint = resultPrint[int(resultPrint.find(']')) + 1:int(len(resultPrint))]
     # se separa el string por ';' en elementos de un arreglo
-    print(resultPrint,"resultPrint3")
     resultArray=resultPrint.split(";")
     resultArray.remove("")
-    print(resultArray,"este es resultArray")
     # Se recorre el resto del string pasado por ais para obtener la clasificacion con su precision de cada objeto
     loadAis(resultArray, aisContain, threshold, folderData)
     #readJson(aisContain,folderStAis)
@@ -216,11 +211,12 @@ def loadDicYOLO(folderYOLO, yoloContain):
     #   folderYOLO: localizacion de las carpetitas de los json que genera la deteccion y clasificacion de la red yolo
     #   yoloContain: instancia de la clase ContainerRN
     print(folderYOLO)
-    for root, dirs, files in os.walk(folderYOLO):
-        print(files)
+    for root, dirs, files in os.walk(folderYOLO+'//events//sheets//'):
+        #print(files,'files')
+        dirs[:] = [d for d in dirs if not d.format('4:d-2:d-2d')]
         for name in files:
-            if str(name).endswith('1280x720.json'):
-                print(name, "names in loadDicYOLO")
+            if str(name).endswith('.json'):
+                #print(name, "names in loadDicYOLO")
                 labelObj=getLabelDicYolo(str(name).split("_")[3], yoloContain)
                 value = int(yoloContain.dict.get(labelObj) or 0)
                 checkLabel(labelObj,value,yoloContain)
@@ -239,7 +235,7 @@ def runAlgYolo(dirRunYolo, dirStoreYolo,yoloContain, dirVideoIn):
     #   dirStoreYolo: direccion de donde se guardaran las carpetitas de todos los objetos json, correspondientes a cada tb
     #   yoloContain: instancia de la clase ContainerRN
     # corre el batch file que hizo juan para correr la red
-    #p=subprocess.Popen(paramYolo)
+    ###p=subprocess.Popen(paramYolo)
     setBatFileYolo(dirRunYolo,dirStoreYolo, dirVideoIn)
     p=subprocess.Popen([r''+dirRunYolo])
     # calcula el tiempo inicial
@@ -259,6 +255,43 @@ def printValues(yoloContain, aisContain):
     #   yoloContain: instancia de la clase ContainerRN, correspondiente al contenido de yolo
     #   aisContain: idem anterior, correspondiente al contenido de ais
     print("{1:^36s}".format("|", "____________________________________"))
+    print("{4}{0:10s}{4}{4}{2:^10s}{4}{4}{3:^10s}{4}".format(" ", "\n", "YOLO", "AIS", "|"))
+    print("{2}{0:^10s}{2}{2}{1:^22s}{2}".format("Etiqueta", "Valor", "|"))
+    with open('compareResult.csv', 'w') as csvfile:
+        #rowNames = ['Label','YOLO','AIS','Time_YOLO','Time_AIS']
+        rowNames = ['Label', 'YOLO', 'AIS']
+        writer = csv.DictWriter(csvfile, fieldnames=rowNames)
+        writer.writeheader()
+        #writer.writerows([{'Time_YOLO': yoloContain.finalTime, 'Time_AIS': aisContain.finalTime}])
+        for label in LABELS:
+            writer.writerows([{'Label': label, 'YOLO': int(yoloContain.dict.get(label) or 0),
+                               'AIS': int(aisContain.dict.get(label) or 0) }])
+
+            valorYolo = int(yoloContain.dict.get(label) or 0)
+            valorAis = int(aisContain.dict.get(label) or 0)
+            print("{2}{0:^10s}{2}{2}{1:^10d}{2}{2}{3:^10d}{2}".format(label, valorYolo, "|", valorAis))
+
+        # end for
+        labelsYolo = set.difference(set(yoloContain.dict.keys()), LABELS)
+        # por todas las etiquetas diferentes que posee yolo y no se encuentra en labels, las imprime con su valor
+        for labelYolo in labelsYolo:
+            writer.writerows([{'Label': labelYolo, 'YOLO': int(yoloContain.dict.get(labelYolo) or 0),
+                               'AIS': 0}])
+
+            print("{2}{0:^10s}{2}{2}{1:^10d}{2}{2}{3:^10d}{2}".format(label, yoloContain.dict.get(label), "|", 0))
+
+        # end for
+
+        writer.writerows([{'Label': 'Total_time', 'YOLO': yoloContain.finalTime, 'AIS': aisContain.finalTime}])
+
+        print("{1:^36s}".format("|", "____________________________________"))
+        print("{0}{1:^10s}{0}{0}{2:^10d}{0}{0}{3:^10d}{0}".format("|", "Total", yoloContain.amount, aisContain.amount))
+        print("{0}{1:^10s}{0}{0}{2:^10f}{0}{0}{3:^10f}{0}".format("|", "Tiempo", yoloContain.finalTime,
+                                                                  aisContain.finalTime))
+
+    # end with
+    """
+    print("{1:^36s}".format("|", "____________________________________"))
     print("{4}{0:10s}{4}{4}{2:^10s}{4}{4}{3:^10s}{4}".format(" ","\n","YOLO","AIS","|"))
     print("{2}{0:^10s}{2}{2}{1:^22s}{2}".format("Etiqueta","Valor","|"))
     # por todos los items dentro de la lista de labels, extraigo el valor que guarda cada diccionario, correspondiente a esa key
@@ -275,6 +308,7 @@ def printValues(yoloContain, aisContain):
     print("{1:^36s}".format("|", "____________________________________"))
     print("{0}{1:^10s}{0}{0}{2:^10d}{0}{0}{3:^10d}{0}".format("|", "Total", yoloContain.amount, aisContain.amount))
     print("{0}{1:^10s}{0}{0}{2:^10f}{0}{0}{3:^10f}{0}".format("|", "Tiempo", yoloContain.finalTime, aisContain.finalTime))
+    """
 # end function
 
 ####################################################################################################################################
@@ -326,7 +360,7 @@ def main():
     parser.add_argument('-dirA', default=os.getcwd() + '//run-ScriptAis-Py.bat', type=str)
     parser.add_argument('-dirStA', default='F://YOLO//Prueba_V1//2018-08-05//', type=str)
     parser.add_argument('-dirY', default=os.getcwd() + '//run-S2-Yolo3-w20.bat', type=str)
-    parser.add_argument('-dirStY', default='D:\TEST_YOLO\Yolo_S2w20T7\events\sheets', type=str)
+    parser.add_argument('-dirStY', default='F://YOLO//TestYoloAis//test_yolo//Yolo_S2w20T7//', type=str)
     parser.add_argument('-video', default='F://YOLO//Prueba_V1//2019-08-05_16-09-46.mp4', type=str)
     parser.add_argument('-timeDetAis', default='F://YOLO//Prueba_V1//datos.txt', type=str)
     parser.add_argument('-um', default=0.85,type=float)
