fd495f016a8b5a0c83bb4a76063d20d32fa7b217
unknown <camaras-server@CAMARAS-SERVER.pladema.net>
2020-05-04 09:56:03 -0300

    Hecho error desde clasificacion de ais, lo lee y no hace nada, en vez de saltear las imagenes que pueden causar error

--- compareRNs.py
+++ compareRNs.py
@@ -6,19 +6,22 @@ import csv
 import argparse
 
 PATH_RUN_BAT_NEURAL_NET="F://YOLO//TestYoloAis//run-ScriptAis-RN1y2.bat"
-PATH_TEST_IMAGES="D://Leo_Test_1//test_images//truck//"
+PATH_TEST_IMAGES="D://Leo_Test_1//test_images//van//"
 LABELS_RN6=list()
 LABELS_RN124=list()
 #Guardan en diccionario la cantidad de clasificaciones de los labels correspondientes a r1 y a r2
 DICT_GT=dict()
-DICT_RN6=dict()
-DICT_RN124=dict()
+DICT_RN6_1RO=dict()
+DICT_RN124_1RO=dict()
+DICT_RN6_2DO=dict()
+DICT_RN124_2DO=dict()
 SUBPROCESS_R1=subprocess
 SUBPROCESS_R2=subprocess
-THRESHOLD_MIN=15.0
-THRESHOLD_MAX=35.0
-LABEL_TEST='truck'
+THRESHOLD_MIN=50.0
+THRESHOLD_MAX=10.0
+LABEL_TEST='van'
 LIST_WRONG_LABEL=list()
+
 
 
 def loadLabels(listLabels,RN6,RN124):
@@ -71,36 +74,6 @@ def setBatFileAis(pathRunBat,pathImg,pathRN,pathTestImg,nameRN):
     # end with
 # end function
 
-"""
-def runNeuralNet(path_img,pathRN,pathTestImg,nameRN):
-    setBatFileAis(PATH_RUN_BAT_NEURAL_NET,path_img,pathRN,pathTestImg,nameRN)
-    sub1 = subprocess.Popen([r''+PATH_RUN_BAT_NEURAL_NET],stdin=subprocess.PIPE,stdout=subprocess.PIPE, shell=False)
-    return sub1
-# end function
-
-def wrAndReadRN(outputRN):
-    #sub.stdin.write(path_img)
-    resultPrint = (str(outputRN).encode("utf-8").decode("unicode-escape").encode("latin-1").decode("utf-8"))
-    resultPrint = resultPrint.replace(r"\r\n", "")
-    resultPrint = resultPrint[int(resultPrint.find('[')):int(len(resultPrint))]
-    print(resultPrint)
-    #if LABELS_R1.clear():
-    #    loadLabels(resultPrint,LABELS_R1)
-    #resultArray = resultPrint[int(resultPrint.find(']')) + 1:int(len(resultPrint))].split(";").remove("")
-#end function
-
-def runNeuralNet2(path_img,pathRN,pathTestImg,nameRN):
-    setBatFileAis(PATH_RUN_BAT_NEURAL_NET, path_img,pathRN,pathTestImg,nameRN)
-    sub = subprocess.Popen([r''+PATH_RUN_BAT_NEURAL_NET],stdout=subprocess.PIPE, shell=False)
-    resultPrint = (str(sub.stdout.read()).encode("utf-8").decode("unicode-escape").encode("latin-1").decode("utf-8"))
-    resultPrint = resultPrint.replace(r"\r\n", "")
-    resultPrint = resultPrint[int(resultPrint.find('[')):int(len(resultPrint))]
-    print(resultPrint)
-    if LABELS_R2.clear():
-        loadLabels(resultPrint,LABELS_R2)
-    #resultArray = resultPrint[int(resultPrint.find(']')) + 1:int(len(resultPrint))].split(";").remove("")
-#end function
-"""
 def initSubprocess(pathFolderRN,pathRunRN,RN):
     stdin=()
     stdout=()
@@ -137,58 +110,49 @@ def initSubprocess(pathFolderRN,pathRunRN,RN):
 
 def classifyImage(folderImage, classify, rn124):
     arrayClassify=classify.strip().replace(";","").split("|")
-    label=""
-    preciss=0.0
+    label_1ro=str()
+    label_2do=str()
+    preciss_1ro=float()
+    preciss_2do=float()
     if arrayClassify.__sizeof__().__ge__(1):
         firstPos = arrayClassify[0].split("_")
-        label = firstPos[0]
-        preciss = float(firstPos[1])
+        label_1ro = firstPos[0]
+        preciss_1ro = float(firstPos[1])
+        if preciss_1ro.__le__(THRESHOLD_MIN) and arrayClassify.__sizeof__().__ge__(2):#si el primero es menor a 50
+            secondPos = arrayClassify[1].split("_")
+            label_2do= secondPos[0]
+            preciss_2do = float(secondPos[1])
         if rn124:
             arrclass = folderImage.split("//")
             root=list(arrclass).pop(list(arrclass).__len__()-2) #root se vuelve el anteultimo elemento del arreglo que contiene
             #los elementos del path del folder, entonces se vuelve la etiqueta original
-            if root.__ne__(label):
-               LIST_WRONG_LABEL.append([folderImage,label,preciss])
-
-    """
-    #para comparar la precission de los labels
-    if arrayClassify.__sizeof__().__ge__(2):
-        firstPos=arrayClassify[0].split("_")
-        secondPos=arrayClassify[1].split("_")
-        firstLabel=firstPos[0]
-        firstPress=float(firstPos[1])
-        secondLabel=secondPos[0]
-        secondPress=float(secondPos[1])
-        if float(abs(firstPress-100.0)).__le__(THRESHOLD_MIN):
-            
-    """
-    """
-    #Recorre todas las posicciones de arrayClassify, para luego comparar
-    for pos in arrayClassify:
-        if pos.__ne__(''):
-            arrLabel=pos.split("_")
-            if label.__eq__("") or label.__ne__("") and arrLabel[1].__ge__(porc):
-                porc=arrLabel[1]
-                label=arrLabel[0]
-            # end if
-        # end if
-    # end for
-    """
-    return label
+            if root.__ne__(label_1ro):
+               LIST_WRONG_LABEL.append([folderImage,label_1ro,preciss_1ro])
+    return label_1ro,label_2do
 # end function
 
 def loadDicc(classify, classRN):
-    if classRN.startswith("RN6000"):
-        DICT_RN6[classify]=(DICT_RN6.get(classify) or 0)+1
-    elif classRN.startswith("RN124000"):
-        DICT_RN124[classify] = (DICT_RN124.get(classify) or 0) + 1
-    else:
-        #print(classify.split("\\"))
-        arrclass=classify.split("\\")
-        label=arrclass.pop()
+    print(classify)
+    if classRN.__eq__('GT'):
+        # print(classify.split("\\"))
+        arrclass = classify.split("\\")
+        label = arrclass.pop()
         print(label)
-        DICT_GT[LABEL_TEST]=(DICT_GT.get(LABEL_TEST) or 0) + 1
-    #end if
+        DICT_GT[LABEL_TEST] = (DICT_GT.get(LABEL_TEST) or 0) + 1
+    else:
+        label_1ro,label_2do=classify
+        if label_1ro:
+            if classRN.startswith("RN6000"):
+                DICT_RN6_1RO[label_1ro]=(DICT_RN6_1RO.get(label_1ro) or 0)+1
+            elif classRN.startswith("RN124000"):
+                DICT_RN124_1RO[label_1ro] = (DICT_RN124_1RO.get(label_1ro) or 0) + 1
+            #end if
+        if label_2do:
+            if classRN.startswith("RN6000"):
+                DICT_RN6_2DO[label_2do]=(DICT_RN6_2DO.get(label_2do) or 0)+1
+            elif classRN.startswith("RN124000"):
+                DICT_RN124_2DO[label_2do] = (DICT_RN124_2DO.get(label_2do) or 0) + 1
+            #end if
 
 #end function
 
@@ -217,19 +181,20 @@ def classifyFolder(stdin,stdout,ClassRN,GT,RN6,RN124):
             # recorre todos los archivos contenidos en files
             for name in files:
                 # encuentra una imagen"
-                if str(name).endswith(".jpg") or str(name).endswith(".jpeg"):
-                    folderImage=root + name
-                    print(folderImage)
-                    line = '{}\n'.format(folderImage)#es importante que este definido el formato del salto de linea, porque eso tambien genera que la herramienta no lo tome como url de imagen, porque no terminaria en .jpg o .jpeg
-                    stdin.write(line)
-                    stdin.flush()
-                    #wrAndReadRN(sub1.stdout.readline())
-                    output = stdout.readline()
-                    print(output.rstrip())
+                #if str(name).endswith(".jpg") or str(name).endswith(".jpeg"):
+                folderImage=root + name
+                print(folderImage)
+                line = '{}\n'.format(folderImage)#es importante que este definido el formato del salto de linea, porque eso tambien genera que la herramienta no lo tome como url de imagen, porque no terminaria en .jpg o .jpeg
+                stdin.write(line)
+                stdin.flush()
+                #wrAndReadRN(sub1.stdout.readline())
+                output = stdout.readline()
+                print(output.rstrip())
+                if output.rstrip().__ne__(";ERROR"):#Si surgio algun error de la herramienta de clasificacion, no lo procesa
                     if RN124:
-                        loadDicc(root,GT)#metodo que carga el diccionario correspondiente al GT de acuerdo al nombre que figura en el path pasado por parametro
+                       loadDicc(root,GT)#metodo que carga el diccionario correspondiente al GT de acuerdo al nombre que figura en el path pasado por parametro
                     loadDicc(classifyImage(folderImage,output,RN124),ClassRN)#se le manda el folder(que de ahi se podria verificar la clasificacion real)
-                    waiting = stdout.readline()  # waiting que espera la proxima escritura, "lo descarto"
+                waiting = stdout.readline()  # waiting que espera la proxima escritura, "lo descarto"
                 # end if
             # end for
         # end if
@@ -239,23 +204,25 @@ def classifyFolder(stdin,stdout,ClassRN,GT,RN6,RN124):
 # end function
 
 def generateResults():
-    with open(LABEL_TEST+'_v1.0_resultRNs.csv', 'w') as csvfile:
+    with open(LABEL_TEST+'_v2.0NO_resultRNs.csv', 'w') as csvfile:
         #rowNames = ['Label','YOLO','AIS','Time_YOLO','Time_AIS']
-        rowNames = ['Label', 'GT', 'RN6000', 'RN124000']
+        rowNames = ['Label', 'GT', 'RN6000-Mayor50', 'RN6000-Menor50', 'RN124000-Mayor50', 'RN124000-Menor50']
         csv.register_dialect('delimiter',delimiter=';',lineterminator='\n')
         writer = csv.DictWriter(csvfile, fieldnames=rowNames, dialect='delimiter')
         writer.writeheader()
         #writer.writerows([{'Time_YOLO': yoloContain.finalTime, 'Time_AIS': aisContain.finalTime}])
         for label in LABELS_RN124:
             writer.writerows([{'Label': label, 'GT': int(DICT_GT.get(label) or 0),
-                               'RN6000': int(DICT_RN6.get(label) or 0),
-                               'RN124000': int(DICT_RN124.get(label) or 0)}])
+                               'RN6000-Mayor50': int(DICT_RN6_1RO.get(label) or 0),
+                               'RN6000-Menor50': int(DICT_RN6_2DO.get(label) or 0),
+                               'RN124000-Mayor50': int(DICT_RN124_1RO.get(label) or 0),
+                             'RN124000-Menor50':int(DICT_RN124_2DO.get(label) or 0)}])
             #valorYolo = int(yoloContain.dict.get(label) or 0)
             #valorAis = int(aisContain.dict.get(label) or 0)
             #print("{2}{0:^10s}{2}{2}{1:^10d}{2}{2}{3:^10d}{2}".format(label, valorYolo, "|", valorAis))
         for pos in LIST_WRONG_LABEL:
             writer.writerows([{'Label': pos[0], 'GT': pos[1],
-                               'RN6000': pos[2]}])
+                               'RN6000-Mayor50': pos[2]}])
         #writer.writerows([{'Label': 'Total_time', 'YOLO': yoloContain.finalTime, 'AIS': aisContain.finalTime}])
         #print("{1:^36s}".format("|", "____________________________________"))
         #print("{0}{1:^10s}{0}{0}{2:^10d}{0}{0}{3:^10d}{0}".format("|", "Total", yoloContain.amount, aisContain.amount))
