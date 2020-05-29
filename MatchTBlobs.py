f62d56d8b39057d3cbb04a488558d52bc5d1c2be
unknown <camaras-server@CAMARAS-SERVER.pladema.net>
2020-05-29 16:04:30 -0300

    comenzando a ver el tema del matching en el tiempo

--- MatchTBlobs.py
+++ MatchTBlobs.py
@@ -8,6 +8,7 @@ from datetime import datetime
 import json
 import csv
 
+#TIME_INIT_VIDEO=datetime(0,0,0,0)
 RANGE_TIME=0.05 #rango de tiempo que se comparan los tb adyacentes en linea de tiempo ordenados
 PATH_JSON="D://CARO//Colon_Montevideo//testObjetos.json"
 PATH_TBS_FOLDER="D://CARO//Colon_Montevideo//TBlobs//"
@@ -20,15 +21,8 @@ LIST_JSON=list() #lista que va a contener todos los json que se encuentren regis
 def sortJsonList(listJson, atribute):
     #ordena ascendentemente a todos los json segun tiempo de inicio al video
     #print(listJson)
-    modAtribute=""
     def takeInitPos(elem):
-        print(elem[atribute])
-        if elem[atribute].__contains__("."):
-            modAtribute=elem[atribute]
-        else:
-            modAtribute="{0}{1}{2}".format(elem[atribute],".",str(0))
-        print(modAtribute)
-        return datetime.strptime(modAtribute,'%Y-%m-%d %H:%M:%S.%f').time()
+        return datetime.strptime(elem[atribute],'%Y-%m-%d %H:%M:%S.%f').time()
     # end internal function
     listJson.sort(key=takeInitPos)
     #print("#################################### Todos ordenados ######################################################")
@@ -45,10 +39,19 @@ def readAndAddJson(objData):
     objData['color']=color
     objData['shape'] = shape
     objData['snapshot'] = snapshot
+    if not objData['init'].__contains__("."):#si no tienen milisegundos, se los "setea en 0"
+        objData['init'] = "{0}{1}{2}".format(objData['init'], ".", str(0))
+    if not objData['finish'].__contains__("."):#si no tienen milisegundos, se los "setea en 0"
+        objData['finish'] = "{0}{1}{2}".format(objData['finish'], ".", str(0))
     LIST_JSON.append(objData)#agrega el objeto json encontrada al final
 # end function
 
 def readAndSortJsons(dirJsons):
+    #Toma del archivo json pasado por parametro, el atributo "data" que es un tipo lista/arreglo y lo recorre
+    #por cada "elemento" dentro de data, los manda a parsear, ya que son objetos tipo json y sus atributos se tienen
+    #que pasar de string a json o hacer modificaciones correspondientes que se encarga la funcion readAndAddJson
+    #que meramente coloca cada objeto json encontrado en data (ya modificado y todos) y los agrega de LIST_JSON
+    #acá se la termina de ordenar ascendentemente por tiempo de inicio a escena.
     with open(dirJsons,'r') as fileJson:
         allJsons=json.loads(fileJson.read()) #lee toddo lo contenido en fileJson y lo parsea a un objeto json, guardandolo
         #en allJsons
@@ -57,14 +60,33 @@ def readAndSortJsons(dirJsons):
             readAndAddJson(pos)
         # end for
     # end with
-    sortJsonList(LIST_JSON, 'init')
-    print(LIST_JSON)
+    sortJsonList(LIST_JSON, 'init')#lista de tblobs tomados como objetos JSON, ordenados por el tiempo de inicio a escena
+    #if LIST_JSON:
+     #   TIME_INIT_VIDEO=LIST_JSON(0)['init']
+    #print(LIST_JSON)
+# end function
+
+def totalTime(tblob):
+    return tblob['init']
+# end function
+
+def foundedTimeMacth(tblob):
+    for anotherTblob in (x for x in LIST_JSON if x['init'] in (tblob['init']-RANGE_TIME,tblob['init'],tblob['finish']+RANGE_TIME)):
+        print(tblob," match con ",anotherTblob)
 
 # end function
 
+def matchingTblobs():
+    for tblob in LIST_JSON:
+        foundedTimeMacth(tblob)
+
+
+
 def main():
     print('holi')
-    readAndSortJsons(PATH_JSON)
+    readAndSortJsons(PATH_JSON)#llama al metodo que lee todos los objetos json encontrados en el archivo .json y los
+    #agrega a LIST_JSON ordenados ascendentemente por tiempo de inicio a escena
+    matchingTblobs()
 
 if __name__ == '__main__':
     main()
